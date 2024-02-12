46d8a5b269eabd6a3a7f1575bf63fb9cc6c33847
Pedro Henrique <pedrosales2806@gmail.com>
2024-02-12 16:25:42 -0300

    Organização do código

--- JS/todo.js
+++ JS/todo.js
@@ -1,29 +1,26 @@
 let count = 1;
 
+/// Cria um novo elemento a partir da entrada e o adiciona a lista
 function addElement(){
     let newListElement = document.createElement("li");
+
     let newInputElement = document.createElement("input");
-
-    newListElement.id = "item" + count;
-
     newInputElement.type = "checkbox";
-
     newInputElement.addEventListener("click", TaskDone);
-
     newListElement.append(newInputElement);
 
     let spanText = document.createElement("span");
     spanText.textContent = document.getElementById("task").value;
-
     newListElement.appendChild(spanText);
 
+    newListElement.id = "item" + count;
     document.getElementById("main list").appendChild(newListElement);
 
     document.getElementById("task").value = "";
-
     count+=1;
 }
 
+// Marca a tarefa como concluida
 function TaskDone(event){
     let parentId = event.target.parentElement.id;
 
