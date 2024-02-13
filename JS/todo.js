2a0f663dda007835c6da9baef573cfb6dd37a7bc
Pedro Henrique <pedrosales2806@gmail.com>
2024-02-13 11:13:57 -0300

    Estilização do layout.Melhoria na formatação de texto. Adição de efeitos de transição. Adição de comentários.

--- JS/todo.js
+++ JS/todo.js
@@ -1,6 +1,6 @@
 let count = 1;
 
-/// Cria um novo elemento a partir da entrada e o adiciona a lista
+// Cria um novo elemento a partir da entrada e o adiciona a lista
 function addElement(){
     let newListElement = document.createElement("li");
 
@@ -10,18 +10,17 @@ function addElement(){
     newListElement.appendChild(newInputElement);
 
     let newSpanElement = document.createElement("span");
-    newSpanElement.textContent = document.getElementById("task").value;
+    // Realiza a formatação
+    newSpanElement.textContent = document.getElementById("task").value.trim() + " ";
     newListElement.appendChild(newSpanElement);
 
     let newButtonElement = document.createElement("button");
-
     newButtonElement.innerHTML = "X";
-
     newButtonElement.addEventListener("click", DelElement);
-
     newListElement.appendChild(newButtonElement);
 
     newListElement.id = "item" + count;
+    newListElement.className = "todo-list";
     document.getElementById("main list").appendChild(newListElement);
 
     document.getElementById("task").value = "";
@@ -33,38 +32,16 @@ function TaskDone(event){
     let parentId = event.target.parentElement.id;
 
     if(document.getElementById(parentId).style.textDecoration.toString() === "line-through"){
-        document.getElementById(parentId).style.backgroundColor = "white";
         document.getElementById(parentId).style.textDecoration = "none";
     }
     else {
-        document.getElementById(parentId).style.backgroundColor = "green";
         document.getElementById(parentId).style.textDecoration = "line-through";
     } 
 }
 
+// Deleta o elemento
 function DelElement(event) {
-
     let parentId = event.target.parentElement.id;
 
     document.getElementById(parentId).innerHTML = "";
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
